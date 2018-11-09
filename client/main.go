2eb073e4900caf50ebb507a073b2633d07bc0130
Gregoire Clement <gregoire.clement@epfl.ch>
2018-11-09 14:39:29 +0100

    Client CLI refactored

--- client/main.go
+++ client/main.go
@@ -18,7 +18,7 @@ var request string
 
 func init() {
 	flag.UintVar(&uiPort, "UIPort", 8080, "port for the UI client")
-	flag.StringVar(&dest, "dest", "", "destination for the private message")
+	flag.StringVar(&dest, "dest", "", "destination for the private message or file request")
 	flag.StringVar(&filename, "file", "", "filename to be indexed by the gossiper")
 	flag.StringVar(&msg, "msg", "", "message to be sent")
 	flag.StringVar(&request, "request", "", "request metafile of this hash")
@@ -27,32 +27,44 @@ func init() {
 func main() {
 	flag.Parse()
 
-	var packet packets_client.ClientPacketI
-
-	if msg != "" {
-		packet = &packets_client.PostMessagePacket{
-			Message:     msg,
-			Destination: dest,
-		}
-	} else if filename != "" && dest != "" && request != "" {
-		// TODO
-		// packet =
-	} else if filename != "" && dest == "" && request == "" {
-		// TODO
-		// packet =
-	}
-
+	packet := inputsToPacket(msg, dest, filename, request)
 	if packet == nil {
 		common.HandleAbort("combination of arguments are not meaningful, use -help flag for more details", nil)
 		return
 	}
-
 	// port 0 means that os picks on that is available
 	_, udpConn := utils.ConnectToIpPort(fmt.Sprintf("localhost:%d", 0))
 
 	udpAddr := utils.IpPortToUDPAddr(fmt.Sprintf("localhost:%d", uiPort))
 
 	sendMessage(udpAddr, udpConn, packet)
+}
+
+func inputsToPacket(msg, dest, filename, request string) packets_client.ClientPacketI {
+	var packet packets_client.ClientPacketI = nil
+
+	if msg != "" && dest == "" && filename == "" && request == "" { // sending rumor
+		packet = &packets_client.PostMessagePacket{
+			Message: msg,
+		}
+	} else if msg != "" && dest != "" && filename == "" && request == "" { // sending private message
+		packet = &packets_client.PostMessagePacket{
+			Message:     msg,
+			Destination: dest,
+		}
+	} else if msg == "" && dest != "" && filename != "" && request != "" { // requesting a file
+		packet = &packets_client.RequestFilePacket{
+			Destination: dest,
+			File:        filename,
+			Request:     request,
+		}
+	} else if msg == "" && filename != "" && dest == "" && request == "" {
+		packet = &packets_client.IndexFilePacket{
+			File: filename,
+		}
+	}
+
+	return packet
 }
 
 func sendMessage(udpAddr *net.UDPAddr, udpConn *net.UDPConn, packet packets_client.ClientPacketI) {
